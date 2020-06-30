abdc6f79e6a9538bae119c3390ce8a8aad21df9e
realTobby <tkattanek00@gmail.com>
2020-06-30 16:16:13 +0200

    Interactions added
    - npc
    - npc has dialogue
    - player can interact with the npc
    - dialogue text is displayed

---
+++ game-jam/Assets/Scripts/CharacterInteraction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterInteraction : MonoBehaviour
+{
+    public Animator interactText;
+
+    GameObject nearestInteractionObject = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nearestInteractionObject != null)
+        {
+            if(Input.GetKeyUp(KeyCode.F))
+            {
+                nearestInteractionObject.GetComponent<NPC>().TriggerDialogue();
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "npc")
+        {
+            interactText.SetBool("IsOpen", true);
+            nearestInteractionObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        nearestInteractionObject = null;
+        interactText.SetBool("IsOpen", false);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "portal")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ game-jam/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public Text NPCName;
+    public Text currentText;
+
+    public Animator myAnimator;
+
+    private Queue<string> dialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialog = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartDialogue(Dialogue data)
+    {
+        myAnimator.SetBool("IsOpen", true);
+
+        NPCName.text = data.name;
+
+        dialog.Clear();
+
+        foreach(string text in data.sentences)
+        {
+            dialog.Enqueue(text);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(dialog.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string text = dialog.Dequeue();
+        currentText.text = text;
+    }
+
+    public void EndDialogue()
+    {
+        myAnimator.SetBool("IsOpen", false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
