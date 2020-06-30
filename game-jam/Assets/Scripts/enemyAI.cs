6b1a53de12d0d9f0238d6ce838d19a9d5023dbc4
Yngvvar <igorBinczak@gmail.com>
2020-06-30 12:47:40 +0200

    updated AI
    moves when idle

--- game-jam/Assets/Scripts/enemyAI.cs
+++ game-jam/Assets/Scripts/enemyAI.cs
@@ -20,8 +20,12 @@ public class enemyAI : MonoBehaviour
     bool isAgro = false;
     Vector3 target;
     Vector3 newPosytion;
+    Vector3 randomNewPos;
     Rigidbody enemyRB;
-
+    int time;
+    int sceneTime;
+    int timeOfMovment;
+    bool canRandomlyMove = false;
 
 
 
@@ -30,28 +34,63 @@ public class enemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        time = sceneTime + Random.Range(2,5);
         enemyRB = enemy.GetComponent<Rigidbody>();
+        randomNewPos = new Vector3(enemy.transform.position.x + Random.Range(-5, 5), enemy.transform.position.y, enemy.transform.position.z + Random.Range(-5, 5)) - enemy.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         spotingPlayer();
+        sceneTime = Mathf.RoundToInt(Time.timeSinceLevelLoad);
 
         if (isAgro && Vector3.Distance(enemy.transform.position, player.transform.position) > 2)
         {
             Vector3 newDirection = Vector3.RotateTowards(enemy.transform.forward, player.transform.position - enemy.transform.position, turnSpeed * Time.deltaTime, 0);
-            print(newDirection);
+            //print(newDirection);
             enemy.transform.rotation = Quaternion.LookRotation(new Vector3(newDirection.x, 0, newDirection.z));
             enemyRB.velocity = enemy.transform.forward * moveSpeed;
             if (Vector3.Distance(enemy.transform.position, player.transform.position) > 2 * viewDistance)
             {
                 isAgro = false;
+                time = sceneTime + Random.Range(2, 5);
+            }
+        }
+        else
+        {
+            if (time == sceneTime)
+            {
+                //print("can move");
+                canRandomlyMove = true;
+                timeOfMovment = sceneTime;
+            }
+        }
+        randomMovment();
+    }
+
+    void randomMovment()
+    {
+        if (canRandomlyMove)
+        {
+            
+            Vector3 newDirection = Vector3.RotateTowards(enemy.transform.forward, randomNewPos, turnSpeed * Time.deltaTime, 0);
+            //print(newDirection);
+            enemy.transform.rotation = Quaternion.LookRotation(new Vector3(newDirection.x, 0, newDirection.z));
+            enemyRB.velocity = enemy.transform.forward * moveSpeed/3;
+            if (timeOfMovment + 0.5f < sceneTime)
+            {
+                canRandomlyMove = false;
+                time = sceneTime + Random.Range(2, 5);
+
+                randomNewPos = new Vector3(enemy.transform.position.x + Random.Range(-5, 5), enemy.transform.position.y, enemy.transform.position.z + Random.Range(-5, 5)) - enemy.transform.position;
+                
             }
         }
     }
 
-    void spotingPlayer() {
+    void spotingPlayer()
+    {
         if (Physics.CheckSphere(enemy.transform.position, viewDistance, layer))
         {
             print("enemy spoted");
